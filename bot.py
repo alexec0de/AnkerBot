5f5dafbd9ee39b75a966ac4a6a2d84dcd1ace810
alexec0de <102225090+alexec0de@users.noreply.github.com>
2022-06-01 17:54:04 +0300

    1

--- bot.py
+++ bot.py
@@ -113,10 +113,10 @@ async def help(ctx):
 	emd = discord.Embed(title = 'Модиративные команды', description = '{}clear - Отчистка чата\n{}kick - Удаление пользователя из сервера\n{}ban - Ограничение доступа к серверу пользователя\n{}mute - Ограничение доступа к чату серверa\n{}eval - Штука для разроботчика'.format(PREFIX, PREFIX, PREFIX, PREFIX, PREFIX))
 	
 	emd2 = discord.Embed(title = 'Основные команды', description = f'{PREFIX}time - Узнать время\n{PREFIX}server - Узнать инфу о сервере')
-	emd3 = discord.Embed(title = 'Игры', description = f'{PREFIX}8ball - Игра про макический шарик\n{PREFIX}coin -Орёл и Решка')
+	emd3 = discord.Embed(title = 'Игры', description = f'{PREFIX}8ball - Игра про макический шарик')
 	emd4 = discord.Embed(title= 'Экономика', description = f'{PREFIX}cash - Узнать свой баланс\n{PREFIX}award - Добавить пользователю баланс')
-	emd5 = discord.Embed(title= 'Музыка', description = f'{PREFIX}join - Присоедится к каналу\n{PREFIX}leave - Выйти из канала\n{PREFIX}play - Сыграть музыку\n{PREFIX}pause - Остановить музыку\n{PREFIX}resume - Продолжить проигрование')
-	emds = [emd, emd2, emd3, emd4, emd5]
+
+	emds = [emd, emd2, emd3, emd4]
 	
 	message = await ctx.send(embed = emd)
 	page = Paginator(client, message, only=ctx.author, use_more = False, embeds = emds)
@@ -169,12 +169,11 @@ async def cash(ctx, member: discord.Member = None):
 
 
 @client.command()
+@commands.has_permissions(manage_messages=True)
 async def award(ctx, member: discord.Member, cash:int):
-	if ctx.author.id == 707241794336718891:
-		await db.update_member("UPDATE users SET balance = balance + ? WHERE member_id = ? AND guild_id = ?", [cash, member.id, ctx.guild.id])
-		await ctx.message.add_reaction("💖")
-	else:
-		await ctx.send('Эта команда только для разраба')
+	await db.update_member("UPDATE users SET balance = balance + ? WHERE member_id = ? AND guild_id = ?", [cash, member.id, ctx.guild.id])
+	await ctx.message.add_reaction("💖")
+
 
 
 #eval 
@@ -233,144 +232,9 @@ async def ball(ctx, ques=""):
 		await ctx.send(f" {random.choice(choices)}")
 
 #coin
-@client.command() 
-async def coin(ctx):
-    Coin = {
-        0: "***Орел***",
-        1: "***Решка***",
-    }
-    side_of_the_coin = random.randint(1, 1000)
-    await ctx.send(f'{Coin[side_of_the_coin%2]}')
+#soon
 
-#music
-#join
-@client.command()
-async def join(ctx):
-    global voice
-    try:
-        if ctx.message.author.voice == None:
-            await ctx.send(f'{ctx.message.author.mention}, может сначала ты на канал зайдешь?')
-            return
 
-        channel = ctx.author.voice.channel
-        voice = await channel.connect()
-    except:
-        await ctx.send(f'Ну не кричи ты так, тут я, тут...')
-        channel = ctx.author.voice.channel
-        voice = await channel.connect()
-
-#leave
-@client.command()
-async def leave(ctx):
-    global voice
-    try:
-        #TrackQueue_1.clean()
-        await ctx.voice_client.disconnect()
-    except AttributeError:
-        await ctx.send(f'Да ушел я уже, ушел, что ты такой злой?...')
-
-#pause
-@client.command()
-async def pause(ctx):
-	voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
-	if voice.is_playing():
-		voice.pause()
-	else:
-		await ctx.send('Вы не можите остановить музыку')
-		
-#resume
-@client.command()
-async def resume(ctx):
-	voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
-	if voice.is_paused():
-		voice.resume()
-	else:
-		await ctx.send('Вы не можите включить музыку')
-
-@client.command()
-async def play(ctx, url: str):
-
-    def check_queue():
-        Queue_infile = os.path.isdir("./Queue")
-        if Queue_infile is True:
-            DIR = os.path.abspath(os.path.realpath("Queue"))
-            length = len(os.listdir(DIR))
-            still_q = length - 1
-            try:
-                first_file = os.listdir(DIR)[0]
-            except:
-                print("No more queded song(s)\n")
-                queues.clear()
-                return
-            main_location = os.path.dirname(os.path.realpath(__file__))
-            song_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first_file)
-            if length != 0:
-                print("Song done, playing next queued\n")
-                print(f"Songs still in queue: {still_q}")
-                song_there = os.path.isfile("song.mp3")
-                if song_there:
-                    os.remove("song.mp3")
-                shutil.move(song_path, main_location)
-                for file in os.listdir("./"):
-                    if file.endswith(".mp3"):
-                        os.rename(file, 'song.mp3')
-                voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
-                voice.source = discord.PCMVolumeTransformer(voice.source)
-                voice.source.volume = 0.07
-            else:
-                queues.clear()
-                return
-        else:
-            queues.clear()
-            print("No songs were queued before the ending of the last song\n")
-    previous_song = os.path.isfile("song.mp3")
-    try:
-        if previous_song:
-            os.remove("song.mp3")
-            queues.clear()
-            print("Removed old song file")
-    except PermissionError:
-        print('Trying to delete song file, but it is playing')
-        await ctx.send("Silly {0.display_name}, I am playing this song now".format(ctx.author))
-        return
-    
-    Queue_infile = os.path.isdir("./Queue")
-
-    try:
-        Queue_folder = ("./Queue")
-        if Queue_infile is True:
-            print("Removed old queue folder")
-            shutil.rmtree(Queue_folder)
-    except:
-        print("No old Queue folder")
-
-    await ctx.send("Braum is on the job!") 
-
-    voice = get(client.voice_clients, guild = ctx.guild)
-    ydl_opts = {
-         'format': 'bestaudio/best',
-         'quiet': True,
-         'postprocessors':[{
-            'key':'FFmpegExtractAudio',
-            'preferredcodec':'mp3',
-            'preferredquality':'192',
-        }]
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        print('Downloading audio now\n')
-        ydl.download([url])
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            name = file
-            print(f"Renamed File: {file}\n")
-            os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
-    voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 0.07
-
-    nname = name.rsplit("-", 2)
-    await ctx.send(f"Playing: {nname}")
-    print("Playing\n")
 
 #error
 @clear.error
